854d58e32f12962fc3f80c14ba8d33e57a382059
EasyLifeQA <agbenga247@gmail.com>
2024-09-15 06:28:07 +0100

    first commit

--- task.js
+++ task.js
@@ -1,1 +1,15 @@
-const profession = "MEdical Laboratory Science"
+//even Number
+//2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+
+//even Number
+//1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21
+
+const number = 5
+    if(number % 2 === 0) {
+        console.log(number + 'even')
+    } 
+    else {
+        console.log(number + 'odd')
+    }
+
+
